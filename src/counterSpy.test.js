620583d887bd9367a361babae5ee6f5d511da954
Sofia Kovalevskaia <SofKov@users.noreply.github.com>
2021-07-23 07:47:09 +0100

    updated tests

--- src/counterSpy.test.js
+++ src/counterSpy.test.js
@@ -24,3 +24,17 @@ test('it returns the array element that does not match str S, P, or Y', () => {
 // spec criteria mentions returning a new array
 // need to filter out any character not matching "S", "P", or "Y" into a new array and return the filtered array as result
 // potential edge case - case sensitivity?
+
+// test 4
+test('it returns the non-spy name - Harriet', () => {
+  expect(counterSpy(['Daryl', 'Harriet', 'James'])).toEqual(['Harriet']);
+});
+
+// current code checks whether array index value exactly equals "s" and "p" and "y"
+// need code to check if array Index value includes certain letters
+// break up names into individual letter, iterate through each letter and check against criteria
+
+// test 5
+test('it returns multiple non-spy names, alphabetically', () => {
+  expect(counterSpy(['Sam','Daryl','Chris','Mauro','Harriet'])).toEqual(['Harriet','Mauro']);
+});
