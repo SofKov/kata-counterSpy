1c8ce0f84d9fdeee1d351241d5000c08e8030280
Sofia Kovalevskaia <SofKov@users.noreply.github.com>
2021-08-20 15:44:57 +0100

    added old solution with old comments

---
+++ src/originalMethods.js
@@ -0,0 +1,29 @@
+const counterSpy = (arr) => {
+       
+    //code for tests 1-3
+    let civiArr = [];   
+    for(let i = 0; i < arr.length; i++) {
+        if ( arr[i] !== 's' && arr[i] !== 'p' && arr[i] !== 'y') {
+            civiArr.push(arr[i]);
+        };
+
+    };
+
+    return civiArr;
+    
+    // code which pushes elements not equaling the test values to new array and returns new array in alphabetical order
+    let civiArr = [];   
+    for(let i = 0; i < arr.length; i++) {
+        let split = arr[i].toLowerCase().split("");
+        if (!split.includes('s') && !split.includes('p') && !split.includes('y')) {
+            civiArr.push(arr[i]);
+        };
+
+        };
+        return civiArr.sort();
+
+    
+    // refactoring above code via filter method and test() regExp method
+    // filter() method? syntax: array.filter((element, index, array) => { ... } )
+
+};
